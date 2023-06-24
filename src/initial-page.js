8c5b08deb454619f2e1f12e2979bd2f339f8b8ec
Ozod <bukhorizodao@gmail.com>
2023-06-24 22:54:57 +0500

    created header and added logo image

--- src/initial-page.js
+++ src/initial-page.js
@@ -1,16 +1,29 @@
-function createHeader(id, text) {
+function createHeader(id, text, imagePath) {
     const header = document.createElement('header');
     header.setAttribute('id', id);
+
+    const headerContainer = document.createElement('div');
+    headerContainer.classList.add('header-container');
+
     const logo = document.createElement('h1');
+    logo.classList.add('welcome-logo');
     logo.textContent = text;
-    header.appendChild(logo);
+
+    const coffeeImage = document.createElement('img');
+    coffeeImage.src = imagePath;
+    coffeeImage.classList.add('coffee-img');
+
+    headerContainer.appendChild(logo);
+    headerContainer.appendChild(coffeeImage);
+
+    header.appendChild(headerContainer);
     return header;
 }
 
 function loadPage() {
     const content = document.querySelector('#content');
 
-    const header = createHeader('header', 'Coffeeland');
+    const header = createHeader('header', 'Coffeeland', './images/coffee.png');
     content.appendChild(header);
 }
 
