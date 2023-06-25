ab5b8a320ef9f3ad7449d8380a20f02c70f36175
Ozod <bukhorizodao@gmail.com>
2023-06-25 17:50:54 +0500

    started with menu container

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import loadPage from "./initial-page";
 import loadHome from "./home";
+import loadMenu from "./menu";
 
 showPage();
 
 function showMenus() {
-    const homeButton = document.querySelector("#button-home");
-    const menuButton = document.querySelector("#button-menu");
-    const contactButton = document.querySelector("#button-contact");
-    const welcomeContainer = document.querySelector(".welcome-container");
+const homeButton = document.querySelector("#button-home");
+const menuButton = document.querySelector("#button-menu");
+// const contactButton = document.querySelector("#button-contact");
+const welcomeContainer = document.querySelector(".welcome-container");
+const coffeeContainer = document.querySelector(".coffee-container");
 
     homeButton.addEventListener("click", () => {
         welcomeContainer.style.display = "flex";
-        loadHome();
+        coffeeContainer.style.display = "none";
+        // loadHome();
     });
 
     menuButton.addEventListener("click", () => {
         welcomeContainer.style.display = "none";
+        coffeeContainer.style.display = "flex";
+        // loadMenu();
     });
     
 }
@@ -24,4 +29,5 @@ function showPage() {
     loadPage();
     loadHome();
     showMenus();
+    loadMenu();
 }
--- src/menu.js
+++ src/menu.js
@@ -12,4 +12,13 @@ function createMenu() {
     coffeeCard.appendChild(coffeeName);
 
     coffeeContainer.appendChild(coffeeCard);
+    
+    return coffeeContainer;
 }
+
+function loadMenu() {
+    const main = document.querySelector('#main');
+    const coffeeContainer = createMenu();
+    main.appendChild(coffeeContainer);
+}
+export default loadMenu;
