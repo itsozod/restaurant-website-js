bc0c76940c54fc78b60e87dff09d87f61c18e1f9
Ozod <bukhorizodao@gmail.com>
2023-06-25 16:58:24 +0500

    added background-image and home page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import loadPage from "./initial-page";
+import loadHome from "./home";
 
 showPage();
 
+function showMenus() {
+    const homeButton = document.querySelector("#button-home");
+    const menuButton = document.querySelector("#button-menu");
+    const contactButton = document.querySelector("#button-contact");
+}
+
 function showPage() {
     loadPage();
+    loadHome();
 }
